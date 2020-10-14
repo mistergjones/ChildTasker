1d8d0a694b67d47fe46a5e09e78d3bacfcccd0f7
Shailesh <shlshkrk@gmail.com>
2020-10-15 10:07:17 +1100

    updated add reward file

--- screens/parent/manageReward/AddReward.js
+++ screens/parent/manageReward/AddReward.js
@@ -24,63 +24,6 @@ const validationSchema = Yup.object().shape({
   category: Yup.object().required().nullable().label("Category"),
 });
 
-// const categories = [
-//   {
-//     backgroundColor: "#fc5c65",
-//     icon: "floor-lamp",
-//     label: "Furniture",
-//     value: 1,
-//   },
-//   {
-//     backgroundColor: "#fd9644",
-//     icon: "car",
-//     label: "Cars",
-//     value: 2,
-//   },
-//   {
-//     backgroundColor: "#fed330",
-//     icon: "camera",
-//     label: "Cameras",
-//     value: 3,
-//   },
-//   {
-//     backgroundColor: "#26de81",
-//     icon: "cards",
-//     label: "Games",
-//     value: 4,
-//   },
-//   {
-//     backgroundColor: "#2bcbba",
-//     icon: "shoe-heel",
-//     label: "Clothing",
-//     value: 5,
-//   },
-//   {
-//     backgroundColor: "#45aaf2",
-//     icon: "basketball",
-//     label: "Sports",
-//     value: 6,
-//   },
-//   {
-//     backgroundColor: "#4b7bec",
-//     icon: "headphones",
-//     label: "Movies & Music",
-//     value: 7,
-//   },
-//   {
-//     backgroundColor: "#a55eea",
-//     icon: "book-open-variant",
-//     label: "Books",
-//     value: 8,
-//   },
-//   {
-//     backgroundColor: "#778ca3",
-//     icon: "application",
-//     label: "Other",
-//     value: 9,
-//   },
-// ];
-//this is add reward file
 function AddReward({ navigation }) {
   const usersContext = useContext(UsersContext);
   const { icons } = usersContext;
