7eb85d9271de209f74d48182e7da52eb4467a6bb
Shailesh <shlshkrk@gmail.com>
2020-11-08 12:10:15 +1100

    updated piechart and user picker

--- components/appButton.js
+++ components/appButton.js
@@ -7,39 +7,41 @@ import colours from "../config/colours.js";
 // import our standard library of colours
 import colors from "../config/colours.js";
 
-const AppButton = ({ title, onPress, color = colours.buttonBackground, isDisabled, }) => {
-    return (
-        <TouchableOpacity
-            style={[styles.button, { backgroundColor: color }, { shadowOpacity: 1 }]}
-            onPress={onPress}
-            disabled={isDisabled}
-        >
-            <Text style={styles.text}>{title}</Text>
-        </TouchableOpacity>
-    );
+const AppButton = ({
+  title,
+  onPress,
+  color = colours.buttonBackground,
+  isDisabled,
+}) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: color }, { shadowOpacity: 1 }]}
+      onPress={onPress}
+      disabled={isDisabled}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        borderRadius: 20,
-        // horizontally and vertically centre the text
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 15,
-        // make button stretch to fill 90% its container
-        width: "90%",
-        margin: 20,
-        borderWidth: 1,
-        borderColor: colours.buttonBorder
-
-
-    },
-    text: {
-        color: colors.buttonText,
-        fontSize: 18,
-        textTransform: "uppercase",
-        fontWeight: "bold",
-
-    },
+  button: {
+    borderRadius: 20,
+    // horizontally and vertically centre the text
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    // make button stretch to fill 90% its container
+    width: "90%",
+    margin: 20,
+    borderWidth: 1,
+    borderColor: colours.buttonBorder,
+  },
+  text: {
+    color: colors.buttonText,
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
 });
 export default AppButton;
--- screens/parent/manageReward/TrackReward.js
+++ screens/parent/manageReward/TrackReward.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 import Screen from "../../../components/appScreen";
 import AppButton from "../../../components/appButton";
 import AppHeading from "../../../components/appHeading.js";
@@ -8,176 +8,173 @@ import { Form } from "../../../components/forms";
 import CategoryPickerItem from "../../../components/appCategoryPickerItem";
 import { UsersContext } from "../../../context/UsersContext";
 import {
-    establishRewardListInObjectFormat,
-    establishKidListInObjectFormat,
-    changeChoresToPieChartDataObject,
+  establishRewardListInObjectFormat,
+  establishKidListInObjectFormat,
+  changeChoresToPieChartDataObject,
 } from "../../../helpers/createObjectLists";
 import AppPicker from "../../../components/appPicker";
 //Added component
 import PieChartWithLabels from "../../../components/PieChartWithLabels";
 import UserPicker from "../../../components/userPicker";
+// import { ScrollView } from "react-native-gesture-handler";
 
 function TrackReward({ navigation }) {
-    const usersContext = useContext(UsersContext);
-    const { kids, rewards, getChoresForKid } = usersContext;
+  const usersContext = useContext(UsersContext);
+  const { kids, rewards, getChoresForKid } = usersContext;
 
-    // KiDS
-    const [selectedKid, setSelectedKid] = useState(null);
+  // KiDS
+  const [selectedKid, setSelectedKid] = useState(null);
 
-    //to get all kids name for dropdown menu
-    var kidList = [];
-    kidList = establishKidListInObjectFormat(kids);
+  //to get all kids name for dropdown menu
+  var kidList = [];
+  kidList = establishKidListInObjectFormat(kids);
 
-    // REWARDS
-    const [selectedReward, setSelectedReward] = useState(null);
+  // REWARDS
+  const [selectedReward, setSelectedReward] = useState(null);
 
-    const [rewardList, setRewardList] = useState([]);
-    const [kidChoresForReward, setKidChoresForReward] = useState([]);
-    const [choresForReward, setChoresForReward] = useState([]); //this will be storing filtered actual chores for the reward
+  const [rewardList, setRewardList] = useState([]);
+  const [kidChoresForReward, setKidChoresForReward] = useState([]);
+  const [choresForReward, setChoresForReward] = useState([]); //this will be storing filtered actual chores for the reward
 
-    const [graphData, setGraphData] = useState();
-    // this function is to set the status of only the kids mapped to kids
-    const handleSelectKid = async (item) => {
-        setSelectedKid(item);
-        let choresForSelectedKid = await getChoresForKid(item.label);
-        console.log("choresForSelectedKid", choresForSelectedKid.length);
-        setKidChoresForReward(choresForSelectedKid);
-        const filteredRewards = getRewardsHaveBeenAssignedToKid(
-            item.label,
-            choresForSelectedKid
-        );
-
-        // set the rewrads list to only contain rewards that have not been assigned for the child selected
-        setRewardList(establishRewardListInObjectFormat(filteredRewards));
-        setSelectedReward(null);
-    };
-
-    const getRewardsHaveBeenAssignedToKid = (kidName, rewardsForKid) => {
-        console.log("rewardsForKid = ", Object.keys(rewardsForKid[0]));
-
-        let filteredRewards = [];
-        let filteredChores = [];
-        // let choresForKid
-        // loop through rewards
-        for (let x = 0; x < rewards.length; x++) {
-            let reward = rewards[x];
-
-            let match = false;
-            // loop through chores
-            for (let i = 0; i < rewardsForKid.length; i++) {
-                if (rewardsForKid[i].rewardID === reward.reward_id) {
-                    match = true;
-                    filteredChores.push({
-                        rewardID: reward.reward_id,
-                        chores: rewardsForKid[i].chores,
-                    });
-                }
-            }
-
-            if (match) {
-                filteredRewards.push(reward);
-            }
-        }
-        setKidChoresForReward(filteredChores);
-        return filteredRewards;
-    };
-    // this function is to set the status of only the kids mapped to kids
-    const handleSelectReward = async (item) => {
-        console.log("item ****" + item.icon);
-        setSelectedReward(item);
-        for (let i = 0; i < kidChoresForReward.length; i++) {
-            if (kidChoresForReward[i].rewardID === item.value) {
-                setChoresForReward(kidChoresForReward[i].chores);
-                const data = changeChoresToPieChartDataObject(
-                    kidChoresForReward[i].chores
-                );
-                console.log("TRACK REWARD", data);
-                setGraphData(data);
-                break;
-            }
-        }
-    };
-
-    return (
-        <Screen>
-            <AppHeading title="Track Reward" />
-            <Form
-                initialValues={{
-                    category_id: "",
-                    category_name: "",
-                    task_id: "",
-                    task_name: "",
-                    task_points: "",
-                    kid_id: "",
-                    kid_name: "",
-                    reward_id: "",
-                    reward_name: "",
-                    reward_Points: "",
-                }}
-            >
-                {/* <AppPicker
-                    items={kidList}
-                    icon="face"
-                    numberOfColumns="2"
-                    placeholder="Select Child"
-                    PickerItemComponent={CategoryPickerItem}
-                    onSelectItem={handleSelectKid}
-                    selectedItem={selectedKid}
-                    width="90%"
-                /> */}
-                <UserPicker
-                    style={styles.picker}
-                    items={kidList}
-                    icon="account-child"
-                    placeholder="Select Child"
-                    onSelectItem={handleSelectKid}
-                    selectedItem={selectedKid}
-                    numberOfColumns={1}
-                />
-
-                {selectedKid && (
-                    <AppPicker
-                        items={rewardList}
-                        icon={selectedReward ? selectedReward.icon : "trophy"}
-                        placeholder="Select Reward"
-                        numberOfColumns="2"
-                        PickerItemComponent={CategoryPickerItem}
-                        onSelectItem={handleSelectReward}
-                        selectedItem={selectedReward}
-                        width="90%"
-                    />
-                )}
-                {selectedReward && (
-                    <PieChartWithLabels
-                        data={graphData}
-                        key={graphData[0].id}
-                    />
-                )}
-                <AppButton
-                    title="Return"
-                    onPress={() => navigation.navigate(screens.ManageRewards)}
-                />
-            </Form>
-        </Screen>
+  const [graphData, setGraphData] = useState();
+  // this function is to set the status of only the kids mapped to kids
+  const handleSelectKid = async (item) => {
+    setSelectedKid(item);
+    let choresForSelectedKid = await getChoresForKid(item.label);
+    console.log("choresForSelectedKid", choresForSelectedKid.length);
+    setKidChoresForReward(choresForSelectedKid);
+    const filteredRewards = getRewardsHaveBeenAssignedToKid(
+      item.label,
+      choresForSelectedKid
     );
+
+    // set the rewrads list to only contain rewards that have not been assigned for the child selected
+    setRewardList(establishRewardListInObjectFormat(filteredRewards));
+    setSelectedReward(null);
+  };
+
+  const getRewardsHaveBeenAssignedToKid = (kidName, rewardsForKid) => {
+    console.log("rewardsForKid = ", Object.keys(rewardsForKid[0]));
+
+    let filteredRewards = [];
+    let filteredChores = [];
+    // let choresForKid
+    // loop through rewards
+    for (let x = 0; x < rewards.length; x++) {
+      let reward = rewards[x];
+
+      let match = false;
+      // loop through chores
+      for (let i = 0; i < rewardsForKid.length; i++) {
+        if (rewardsForKid[i].rewardID === reward.reward_id) {
+          match = true;
+          filteredChores.push({
+            rewardID: reward.reward_id,
+            chores: rewardsForKid[i].chores,
+          });
+        }
+      }
+
+      if (match) {
+        filteredRewards.push(reward);
+      }
+    }
+    setKidChoresForReward(filteredChores);
+    return filteredRewards;
+  };
+  // this function is to set the status of only the kids mapped to kids
+  const handleSelectReward = async (item) => {
+    console.log("item ****" + item.icon);
+    setSelectedReward(item);
+    for (let i = 0; i < kidChoresForReward.length; i++) {
+      if (kidChoresForReward[i].rewardID === item.value) {
+        setChoresForReward(kidChoresForReward[i].chores);
+        const data = changeChoresToPieChartDataObject(
+          kidChoresForReward[i].chores
+        );
+        console.log("TRACK REWARD", data);
+        setGraphData(data);
+        break;
+      }
+    }
+  };
+
+  return (
+    <Screen>
+      <AppHeading title="Track Reward" />
+      <ScrollView style={{ marginTop: 15 }}>
+        <Form
+          initialValues={{
+            category_id: "",
+            category_name: "",
+            task_id: "",
+            task_name: "",
+            task_points: "",
+            kid_id: "",
+            kid_name: "",
+            reward_id: "",
+            reward_name: "",
+            reward_Points: "",
+          }}
+        >
+          <UserPicker
+            style={styles.picker}
+            items={kidList}
+            icon="account-child"
+            placeholder="Select Child"
+            onSelectItem={handleSelectKid}
+            selectedItem={selectedKid}
+            numberOfColumns={1}
+          />
+
+          {selectedKid && (
+            <AppPicker
+              items={rewardList}
+              icon={selectedReward ? selectedReward.icon : "trophy"}
+              placeholder="Select Reward"
+              numberOfColumns="1"
+              PickerItemComponent={CategoryPickerItem}
+              onSelectItem={handleSelectReward}
+              selectedItem={selectedReward}
+              width="90%"
+            />
+          )}
+          {selectedReward && (
+            <PieChartWithLabels data={graphData} key={graphData[0].id} />
+          )}
+        </Form>
+      </ScrollView>
+      <AppButton
+        title="Return"
+        onPress={() => navigation.navigate(screens.ManageRewards)}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    lineGraphContainer: {
-        flexDirection: "row",
-        height: 200,
-        padding: 10,
-        width: "100%",
-    },
-    tabLinks: {
-        marginTop: 15,
-        textAlign: "center",
-        flexDirection: "row",
-        justifyContent: "center",
-    },
-    tab: {
-        margin: 10,
-    },
+  lineGraphContainer: {
+    flexDirection: "row",
+    height: 200,
+    padding: 10,
+    width: "100%",
+  },
+  tabLinks: {
+    marginTop: 15,
+    textAlign: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  tab: {
+    margin: 10,
+  },
+  picker: {
+    marginBottom: 150,
+    // height: 50,
+    width: "90%",
+    alignSelf: "center",
+    backgroundColor: "transparent",
+  },
 });
 
 export default TrackReward;
